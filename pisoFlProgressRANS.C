e7abd3b4c308efd4fa503a3cfab5e77d421e6746
Ville Tossavainen <ville.tossavainen@gmail.com>
2020-06-02 21:53:38 -0700

    refactor: Improved code readability

--- pisoFlProgressRANS.C
+++ pisoFlProgressRANS.C
@@ -43,94 +43,91 @@ Description
 
 int main(int argc, char *argv[])
 {
-# include "setRootCase.H"
-  
-# include "createTime.H"
-# include "createMesh.H"
-# include "createFields.H"
+  #include "setRootCase.H"
 
-# include "readChemTable.H" // read chemtable properties
+  #include "createTime.H"
+  #include "createMesh.H"
+  #include "createFields.H"
 
-//# include "maxTemp.H"
+  #include "readChemTable.H" // read chemtable properties
 
-# include "readPISOControls.H"
-# include "initContinuityErrs.H"
-# include "readTimeControls.H"
-# include "compressibleCourantNo.H"
-# include "setInitialDeltaT.H"
+  //# include "maxTemp.H"
 
-// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+  #include "readPISOControls.H"
+  #include "initContinuityErrs.H"
+  #include "readTimeControls.H"
+  #include "compressibleCourantNo.H"
+  #include "setInitialDeltaT.H"
+
+  // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
 
   Info<< "\nStarting time loop\n" << endl;
 
   // lduMatrix::debug = 0;
 
   while (runTime.run())
-    {
+  {
+    #include "readTimeControls.H"
+    #include "readPISOControls.H"
+    #include "compressibleCourantNo.H"
+    #include "setDeltaT.H"
 
-#     include "readTimeControls.H"
-#     include "readPISOControls.H"
-#     include "compressibleCourantNo.H"
-#     include "setDeltaT.H"
+    runTime++;
 
-      runTime++;
+    #include "updateProperties.H" // update alpha, wc
+    #include "updateRho.H"
 
-#     include "updateProperties.H" // update alpha, wc
-#     include "updateRho.H"
-
-      Info<< "Time = " << runTime.timeName() << nl << endl;
+    Info<< "Time = " << runTime.timeName() << nl << endl;
 
       // --- PISO loop
       for (int corr=1; corr<=nCorr; corr++)
-        {
+      {
+        #include "rhoEqn.H"
 
-#     include "rhoEqn.H"
+        turbulence->correct();
 
-      turbulence->correct();
+        #include "UEqn.H"
 
-#     include "UEqn.H"
+        #include "zEqn.H"
+        #include "cEqn.H"
+        #include "zVarEqn.H"
+        #include "boundValues.H"
+        #include "updateProperties.H" // update alpha, wc
+        #include "updateRho.H"
 
-#     include "zEqn.H"
-#     include "cEqn.H"
-#     include "zVarEqn.H"
-#     include "boundValues.H"
-#     include "updateProperties.H" // update alpha, wc
-#     include "updateRho.H"
+        psi.oldTime();
+        // psi = W/(R*T);
+        psi = rho / pref;
 
-      psi.oldTime();
-//      psi = W/(R*T);
-      psi = rho / pref;
+        p.storePrevIter();
+        rho.storePrevIter();
 
-      p.storePrevIter();
-      rho.storePrevIter();
+        #include "pEqn.H"
+      }
 
-#     include "pEqn.H"
+    rho = p * psi;
 
-	}
+    Info << "Zmean max: " << max(Zmean).value()
+	       << "\tCmean max: " << max(Cmean).value()
+	       << "\tZvar max: " << max(Zvar).value()
+	       << "\tU max: " << mag(max(U).value())
+         << "\tp max: " << max(p).value()
+      	 << "\trho max: " << max(rho).value()
+        << endl;
 
-      rho = p*psi;
+    Info << "Zmean min: " << min(Zmean).value()
+	       << "\tCmean min: " << min(Cmean).value()
+	       << "\tZvar min: " << min(Zvar).value()
+         << "\tU min: " << mag(min(U).value())
+         << "\tp min: " << min(p).value()
+         << "\trho min: " << min(rho).value()
+         << endl;
 
-      Info << "Zmean max: " << max(Zmean).value()
-	   << "\tCmean max: " << max(Cmean).value()
-	   << "\tZvar max: " << max(Zvar).value()
-	   << "\tU max: " << mag(max(U).value())
-	   << "\tp max: " << max(p).value()
-	   << "\trho max: " << max(rho).value()
-	   << endl;
+    runTime.write();
 
-      Info << "Zmean min: " << min(Zmean).value()
-	   << "\tCmean min: " << min(Cmean).value()
-	   << "\tZvar min: " << min(Zvar).value()
-	   << "\tU min: " << mag(min(U).value())
-	   << "\tp min: " << min(p).value()
-	   << "\trho min: " << min(rho).value()
-	   << endl;
-
-      runTime.write();
-
-      Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
-	  << "  ClockTime = " << runTime.elapsedClockTime() << " s"
-	  << nl << endl;
+    Info << "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+	       << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+	       << nl << endl;
     }
 
   Info<< "End\n" << endl;
